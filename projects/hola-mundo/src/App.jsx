c5c713a7f8ef09ae4609f6b756d3254f080ac267
sebaas256 <csebas220@gmail.com>
2024-11-19 22:59:03 -0600

    practica-1

--- projects/hola-mundo/src/App.jsx
+++ projects/hola-mundo/src/App.jsx
@@ -1,24 +1,41 @@
 import './App.css'
 import { TwitterUsuariosCard } from './TwitterUsuariosCard'
 
+const users = [
+    {
+        userName: 'Gorillaz',
+        name: 'Gorillaz-official',
+        isfollowing: true
+    },
+    {
+        userName: 'Iron Maiden',
+        name: 'Iron Maiden-official',
+        isfollowing: false
+    },{
+        userName: 'elonmusk',
+        name: 'Elon Musk',
+        isfollowing: true
+    },{
+        userName: 'The Beatles',
+        name: 'The Beatles-official',
+        isfollowing: false
+    },
+]
+
 export function App () {
-    return(
-        <section className='App'>
-        <TwitterUsuariosCard userName="Gorillaz">
-        Gorillaz-official 
-        </TwitterUsuariosCard>
-
-        <TwitterUsuariosCard isfollowing userName="Iron Maiden">
-        Iron Maiden-official 
-        </TwitterUsuariosCard>
-
-        <TwitterUsuariosCard userName="elonmusk">
-        Elon Musk 
-        </TwitterUsuariosCard>
-
-        <TwitterUsuariosCard userName="The Beatles">
-        The Beatles-official 
-        </TwitterUsuariosCard>
-        </section>
+    return (
+      <section className='App'>
+        {
+          users.map(({ userName, name, isFollowing }) => (
+            <TwitterUsuariosCard
+              key={userName}
+              userName={userName}
+              initialIsFollowing={isFollowing}
+            >
+              {name}
+            </TwitterUsuariosCard>
+          ))
+        }
+      </section>
     )
-}
+  }
